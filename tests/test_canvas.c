a8595502d864f8bb34858475f8527bd6dd8fe477
Nicholas Saraiva <nicholas.saraivadev@gmail.com>
2026-01-09 13:38:03 +0000

    Initial commit Canvas

---
+++ tests/test_canvas.c
@@ -0,0 +1,12 @@
+#include "../include/minilibx-linux/mlx.h"
+
+int main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 720, 440, "Arroba");
+	mlx_loop(mlx);
+	return (0);
+}
