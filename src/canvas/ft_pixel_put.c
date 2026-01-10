a4fe3272d08613657ec3d27e1b501109013d173c
Nicholas Saraiva <nicholas.saraivadev@gmail.com>
2026-01-10 16:41:24 +0000

    FEAT: Boundary safety

--- src/canvas/ft_pixel_put.c
+++ src/canvas/ft_pixel_put.c
@@ -5,7 +5,12 @@ static unsigned int	construct_color(t_tuple color, int endian);
 void	pixel_put(t_canvas *canvas, int x, int y, t_tuple color)
 {
 	char *dst;
+	int		index;
+	char	*dst;
 
+	if (x > WIDTH - 1 || y > HEIGHT - 1
+		|| x < 0 || y < 0)
+		return ;
 	dst = canvas->address + (y * canvas->sl + x * (canvas->bpp / 8));
 	*(unsigned int*)dst = construct_color(color, canvas->endian);
 }
