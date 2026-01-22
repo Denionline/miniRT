9439a74403cd331aba28696f8f36978de109a4ba
denionline <danielximenes164@gmail.com>
2026-01-22 16:05:58 +0000

    FIX: Correct memory allocation for world objects in default_world function

--- src/world/world_constructors.c
+++ src/world/world_constructors.c
@@ -18,11 +18,16 @@ t_world	default_world(void)
 	s2 = sphere();
 	s2.transform = scaling(0.5f, 0.5f, 0.5f);
 	new_world.light = point_light(point(-10, 10, -10), color_float(1, 1, 1));
-	new_world.objects = ft_calloc(2, sizeof(t_object));
+	new_world.objects = ft_calloc(2, sizeof(t_object *));
 	if (!new_world.objects)
 		exit(404);
-	new_world.objects[0] = &s1;
-	new_world.objects[1] = &s2;
+	new_world.objects[0] = ft_calloc(1, sizeof(t_object));
+	new_world.objects[1] = ft_calloc(1, sizeof(t_object));
+	if (!new_world.objects[0] || !new_world.objects[1])
+		exit(404);
+	*new_world.objects[0] = s1;
+	set_transform(&s2, scaling(0.5f, 0.5f, 0.5f));
+	*new_world.objects[1] = s2;
 	new_world.n_objs = 2;
 	return (new_world);
 }
