a88b8e16b4a446bbecba497a490fed9a2dac0083
Nicholas Saraiva <nicholas.saraivadev@gmail.com>
2026-01-22 16:34:17 +0000

    ADD: Enhance camera structure with half_w and half_h; implement ray_for_pixel function

--- include/world.h
+++ include/world.h
@@ -7,9 +7,12 @@ typedef struct s_camera
 {
 	float		fov;
 	float		pixel_size;
+	float		half_w;
+	float		half_h;
 	t_matrix	transform;
 }	t_camera;
 
 t_matrix	view_transform(t_tuple init_location, t_tuple look_pos, t_tuple up_vector);
 t_camera	camera(float fov);
+t_ray	ray_for_pixel(t_camera cam, float px, float py);
 #endif
--- src/world/camera.c
+++ src/world/camera.c
@@ -1,19 +1,28 @@
 #include "head.h"
 
-
 t_camera	camera(float fov)
 {
 	t_camera	cam;
 	float		aspect;
+	float		half_v;
 
+	half_v = tanf(fov / 2);
 	cam = (t_camera) 
 	{
 		.fov = fov,
 		.transform = identity_matrix()
 	};
 	aspect = WIDTH / HEIGHT;
-	cam.pixel_size = tanf(fov / 2) * 2 / WIDTH;
 	if (aspect < 1)
-		cam.pixel_size *= aspect;
+	{
+		cam.half_w = half_v;
+		cam.half_w = half_v / aspect; 
+	}
+	else
+	{
+		cam.half_w = half_v * aspect;
+		cam.half_w = half_v; 
+	}
+	cam.pixel_size = cam.half_w * 2 / HEIGHT;
 	return (cam);
 }
