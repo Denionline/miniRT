a845ae1dafc8c0b60e289e8797f8e37e83426c3d
denionline <danielximenes164@gmail.com>
2026-01-23 12:29:12 +0000

    feat: Update test

--- tests/0test_create_world.c
+++ tests/0test_create_world.c
@@ -43,14 +43,26 @@
 // 	print_tuple_simple("Color", c);
 // }
 
+// int	main(void) {
+// 	t_world			w = default_world();
+// 	w.light = point_light(point(0, 0.25, 0), color_float(1, 1, 1));
+// 	t_ray			r = ray(point(0, 0, 0), vector(0, 0, 1));
+// 	t_object		*shape = w.objects[1];
+// 	t_intersection	i = intersection(0.5, shape);
+// 	t_computations	comps = prepare_computations(i, r);
+// 	t_tuple			c = shade_hit(w, comps);
+// 	print_world(w);
+// 	print_tuple_simple("\nShaded color", c);
+// }
+
 int	main(void) {
-	t_world			w = default_world();
-	w.light = point_light(point(0, 0.25, 0), color_float(1, 1, 1));
-	t_ray			r = ray(point(0, 0, 0), vector(0, 0, 1));
-	t_object		*shape = w.objects[1];
-	t_intersection	i = intersection(0.5, shape);
-	t_computations	comps = prepare_computations(i, r);
-	t_tuple			c = shade_hit(w, comps);
-	print_world(w);
-	print_tuple_simple("\nShaded color", c);
+	t_world		w = default_world();
+	t_object	*outer = w.objects[0];
+	outer->material.ambient = 1;
+	t_object	*inner = w.objects[1];
+	inner->material.ambient = 1;
+	t_ray		r = ray(point(0, 0, 0.75), vector(0, 0, -1));
+	t_tuple		c = color_at(w, r);
+	print_object(*inner);
+	print_tuple_simple("Color inner is", c);
 }
