7c1b7828326359ce85713fecd302767cc9143caa
denionline <danielximenes164@gmail.com>
2026-01-25 13:18:21 +0000

    feat: Implement plane object constructor

--- src/objects/constructors_objs.c
+++ src/objects/constructors_objs.c
@@ -25,3 +25,18 @@ t_object	*sphere(void)
 	new_object->type = SPHERE;
 	return (new_object);
 }
+
+t_object	*plane(void)
+{
+	t_object	*new_object;
+
+	new_object = ft_calloc(1, sizeof(t_object));
+	if (!new_object)
+		exit(42);
+	new_object->transform = identity_matrix();
+	new_object->position = point(0, 0, 0);
+	new_object->material = material();
+	new_object->type = PLANE;
+	return (new_object);
+
+}
