8176a120882a2bab829dff48f7f550b458277ebf
denionline <danielximenes164@gmail.com>
2026-01-25 15:06:08 +0000

    feat: Add tests for plane rendering and intersection handling

---
+++ tests/0PIT_planes.c
@@ -0,0 +1,60 @@
+#include "head.h"
+
+int	main(void)
+{
+	t_world     w;
+	t_camera    c;
+	t_tuple     from;
+	t_tuple     to;
+	t_tuple     up;
+
+	t_object    *floor;
+	t_object    *middle;
+	t_object    *right;
+	t_object    *left;
+	t_object    *objs[4];
+
+	floor = plane();
+	floor->material = material();
+	floor->material.color = color_float(1, 0.9, 0.9);
+	floor->material.specular = 0;
+
+	middle = sphere();
+	middle->transform = translation(-0.5, 1, 0.5);
+	middle->material = material();
+	middle->material.color = color_float(0.1, 1, 0.5);
+	middle->material.difuse = 0.7;
+	middle->material.specular = 0.3;
+
+	right = sphere();
+	right->transform = multiply_matrix(translation(1.5, 0.5, -0.5), scaling(0.5, 0.5, 0.5));
+	right->material = material();
+	right->material.color = color_float(0.5, 1, 0.1);
+	right->material.difuse = 0.7;
+	right->material.specular = 0.3;
+
+	left = sphere();
+	left->transform = multiply_matrix(translation(-1.5, 0.33, -0.75), scaling(0.33, 0.33, 0.33));
+	left->material = material();
+	left->material.color = color_float(1, 0.8, 0.1);
+	left->material.difuse = 0.7;
+	left->material.specular = 0.3;
+
+	objs[0] = floor;
+	objs[1] = middle;
+	objs[2] = right;
+	objs[3] = left;
+
+	w.light = point_light(point(-10, 10, -10), color_float(1, 1, 1));
+	w.n_objs = 4;
+	w.objects = objs;
+
+	c = camera(500, 500, PI / 3);
+	from = point(0, 1.5, -5);
+	to = point(0, 1, 0);
+	up = vector(0, 1, 0);
+	c.transform = (view_transform(from, to, up));
+	t_canvas canvas = render(c, w);
+	ft_init_hooks(&canvas);
+	mlx_loop(canvas.mlx_ptr);
+}
