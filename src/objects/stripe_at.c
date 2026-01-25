91c7ffd6a7332882600711013cca04cbbc23a8b9
Nicholas Saraiva <nicholas.saraivadev@gmail.com>
2026-01-25 18:43:24 +0000

    fix: Simplify stripe_at function by removing unnecessary calculations

--- src/objects/stripe_at.c
+++ src/objects/stripe_at.c
@@ -2,18 +2,9 @@
 
 t_tuple	stripe_at(t_pattern p, t_tuple pos)
 {
-	int		is_negative;
-	int		div;
-
-	div = 1.0f/2.0f;
-	is_negative = 0;
-	if (pos.x < 0)
-		is_negative = 1;
-	while (pos.x > 2 || pos.x < -2)
-		pos.x *= div;
-	if ((pos.x < 1 && !is_negative) || pos.x < -1)
+	if (!fmod(pos.x, 2))
 		return (p.a);
-	return(p.b);
+	return (p.b);
 }
 
 t_tuple	stripe_at_obj(t_pattern p, t_object obj, t_tuple pos)
