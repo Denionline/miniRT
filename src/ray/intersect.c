35c636f24ce9987197790624562677c77ed79c52
denionline <danielximenes164@gmail.com>
2026-01-25 15:06:01 +0000

    fix: Refactor intersection handling for plane objects

--- src/ray/intersect.c
+++ src/ray/intersect.c
@@ -31,6 +31,10 @@ t_intersections	intersect(t_object *object, t_ray r)
 
 	r = transform_ray(r, inverse(object->transform));
 	inter = (t_intersections) {};
+
+	if (object->type == PLANE)
+		return (local_intersect(object, r));
+
 	vec = vector(dot(r.direction, r.direction), \
 	2 * dot(r.origin, r.direction), dot(r.origin, r.origin) - \
 	(object->diameter * 0.5) * \
