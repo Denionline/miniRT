fcfb49d424cda18f76cc88cd58bcbfe145175b2a
denionline <danielximenes164@gmail.com>
2026-01-28 10:38:07 +0000

    fix: Correct parameter type in parse_light function signature

--- include/scene.h
+++ include/scene.h
@@ -19,7 +19,7 @@ typedef struct s_scene
 void	parse_scene(t_scene *scene, char *file);
 void	parse_amblight(t_amblight *amblight, char *line);
 void	parse_camera(t_camera *camera, char *line);
-void	parse_light(t_world *w, char *line);
+void	parse_light(t_light *l, char *line);
 void	parse_object(t_world *w, char *line, enum object_type type);
 
 t_tuple	string_to_tuple(char *string, enum e_TUPLE_TYPES w);
