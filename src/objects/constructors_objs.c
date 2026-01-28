718fb5992490e7c506a05eef09f981bdf2213102
denionline <danielximenes164@gmail.com>
2026-01-28 18:40:11 +0000

    fix: Set default material color for cone object constructor

--- src/objects/constructors_objs.c
+++ src/objects/constructors_objs.c
@@ -67,7 +67,7 @@ t_object	*cone(void)
 	new_object->position = point(0, 0, 0);
 	new_object->diameter = 2.0f;
 	new_object->height = 1.0f;
-	new_object->material = material();
+	new_object->material = material(color_float(1, 1, 1));
 	new_object->type = CONE;
 	return (new_object);
 }
