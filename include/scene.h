9078857531492f47cc581ca6f1a7128fbdeeff71
denionline <danielximenes164@gmail.com>
2026-01-28 15:33:41 +0000

    fix: Change scene structure members to pointers and update function signatures

--- include/scene.h
+++ include/scene.h
@@ -11,14 +11,14 @@ typedef struct s_amblight
 
 typedef struct s_scene
 {
-	t_world		world;
-	t_amblight	amblight;
-	t_camera	camera;
+	t_world		*world;
+	t_amblight	*amblight;
+	t_camera	*camera;
 }	t_scene;
 
-void	parse_scene(t_scene *scene, char *file);
-void	parse_amblight(t_amblight *amblight, char *line);
-void	parse_camera(t_camera *camera, char *line);
+void	parse_scene(t_scene **scene, char *file);
+void	parse_amblight(t_amblight **amblight, char *line);
+void	parse_camera(t_camera **camera, char *line);
 void	parse_light(t_light *l, char *line);
 void	parse_object(t_world *w, char *line, enum object_type type);
 
