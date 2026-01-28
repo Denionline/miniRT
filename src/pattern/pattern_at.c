f1d0ed039175461ec94a8b0cf1019b8701983985
Nicholas Saraiva <nicholas.saraivadev@gmail.com>
2026-01-28 13:51:15 +0000

    feat: Add checkboard pattern type and update pattern_at function

--- src/pattern/pattern_at.c
+++ src/pattern/pattern_at.c
@@ -1,29 +1,33 @@
 #include "head.h"
 
-t_tuple	pattern_at(t_pattern g, t_tuple p)
+t_tuple	pattern_at(t_pattern pat, t_tuple p)
 {
 	t_tuple	distance;
 	float	fraction;
 
-	if (g.type == RING)
+	if (pat.type == RING)
 	{
 		if (fabs(fmod(floorf(sqrtf(powf(p.x, 2.0f) + powf(p.y, 2))), 2)) < 0.001f) 
-			return (g.a);
-		return (g.b);
+			return (pat.a);
+		return (pat.b);
 	}
-	if (g.type == STRIP)
+	if (pat.type == STRIP)
 	{
 		if (fabs(fmod(floor(p.x), 2.0f)) < 0.0001f) 
-			return (g.a);
-		return (g.b);
+			return (pat.a);
+		return (pat.b);
 	}
-	distance = subtract_tuples(g.b, g.a);
-	fraction = (p.x + 1.0f) / 2.0f;
-	if (fraction < 0) 
-		fraction = 0;
-	if (fraction > 1) 
-		fraction = 1;
-	return (sum_tuples(g.a, multiply_tuple(distance, fraction)));
+	if (pat.type == GRADIENT)
+	{
+		distance = subtract_tuples(pat.b, pat.a);
+		fraction = (p.x + 1.0f) / 2.0f;
+		if (fraction < 0) 
+			fraction = 0;
+		if (fraction > 1) 
+			fraction = 1;
+		return (sum_tuples(pat.a, multiply_tuple(distance, fraction)));
+	}
+	return (color_float(0, 0, 0));
 }
 
 t_tuple	pattern_at_obj(t_pattern p, t_object obj, t_tuple pos)
