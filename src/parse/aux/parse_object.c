3dc468fe9acddcaf08c42426f7b7197af79a69e6
denionline <danielximenes164@gmail.com>
2026-01-28 10:38:18 +0000

    feat: Implement sphere parsing logic in parse_sphere function

--- src/parse/aux/parse_object.c
+++ src/parse/aux/parse_object.c
@@ -1,6 +1,36 @@
 #include "head.h"
 
+static void	parse_sphere(t_object *sphere, char *line)
+{
+	size_t	i;
+
+	i = 2;
+	while (line && line[i])
+	{
+		while (ft_isspace(line[i]))
+			i++;
+		if (is_tuple_empty(sphere->position))
+			sphere->position = string_to_tuple(line + i, POINT);
+		else if (!sphere->diameter)
+			sphere->diameter = ft_atof(line + i);
+		else if (is_tuple_empty(sphere->material.color))
+
+
+
+
+
+
+		while (line[i] && !ft_isspace(line[i++]));
+	}
+}
+
 void	parse_object(t_world *w, char *line, enum object_type type)
 {
-	
-}
+	t_object	*new_object;
+
+	new_object = ft_calloc(1, sizeof(t_object));
+	if (!new_object)
+		exit(42); //Malloc error
+	*new_object = (t_object){};
+	// if (type == SPHERE)
+}	
