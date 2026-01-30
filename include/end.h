2ecebfb0cc87be7089d878fcb71296f791631cb1
denionline <danielximenes164@gmail.com>
2026-01-30 12:09:50 +0000

    fix: Prefix error codes with 'ERR_' for consistency in end.h

--- include/end.h
+++ include/end.h
@@ -2,10 +2,16 @@
 # define END_H
 
 enum ERROR_CODE {
-	MALLOC = 42,
+	ERR_MALLOC = 42,
+	ERR_NOT_ENOUGH_ARGS,
 	ERR_OPEN_FILE,
 	ERR_INVALID_TUPLE,
 	ERR_UNKNOWN_IDENTIFIER,
+	ERR_OUT_OF_RANGE,
+	ERR_INVALID_CHAR,
 };
+
+void	end(t_scene *scene, enum ERROR_CODE status_code);
+void	print_error(enum ERROR_CODE error_code);
 
 #endif
