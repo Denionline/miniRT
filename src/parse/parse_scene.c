0ec4058e017e7b1927b1f3fb26361cd09360cbdf
denionline <danielximenes164@gmail.com>
2026-01-30 15:00:10 +0000

    fix: Improve line parsing in parse_line function for better error handling

--- src/parse/parse_scene.c
+++ src/parse/parse_scene.c
@@ -2,23 +2,25 @@
 
 static void	parse_line(t_scene *scene, char *line)
 {
+	size_t	i;
+
 	if (is_line_empty(line))
 		return ;
-	while (ft_isspace(*line))
-		line++;
-	if (*line == 'A')
-		return (parse_amblight(scene, &scene->amblight, line + 1));
-	if (*line == 'C')
-		return (parse_camera(scene, &scene->camera, line + 1));
-	if (*line == 'L')
-		return (parse_light(scene, &scene->world->light, line + 1));
-	if (*line == 's' && *(line + 1) == 'p')
-		return (parse_object(scene, scene->world, line, SPHERE));
-	if (*line == 'p' && *(line + 1) == 'l')
-		return (parse_object(scene, scene->world, line, PLANE));
-	if (*line == 'c' && *(line + 1) == 'y')
-		return (parse_object(scene, scene->world, line, CYLINDER));
-	end(scene, ERR_UNKNOWN_IDENTIFIER);
+	while (ft_isspace(line[i]))
+		i++;
+	if (line[i] == 'A')
+		return (parse_amblight(scene, &scene->amblight, line + i + 1));
+	if (line[i] == 'C')
+		return (parse_camera(scene, &scene->camera, line + i + 1));
+	if (line[i] == 'L')
+		return (parse_light(scene, &scene->world->light, line + i + 1));
+	if (line[i] == 's' && line[i + 1] == 'p')
+		return (parse_object(scene, scene->world, line + i, SPHERE));
+	if (line[i] == 'p' && line[i + 1] == 'l')
+		return (parse_object(scene, scene->world, line + i, PLANE));
+	if (line[i] == 'c' && line[i + 1] == 'y')
+		return (parse_object(scene, scene->world, line + i, CYLINDER));
+	end(scene, ERR_UNKNOWN_IDENTIFIER, line);
 }
 
 void	parse_scene(t_scene **scene, char *file)
@@ -27,9 +29,9 @@ void	parse_scene(t_scene **scene, char *file)
 	char		*line;
 
 	if (file_fd < 0)
-		end(*scene, ERR_OPEN_FILE);
-	*scene = saffe_calloc(*scene, 1, sizeof(t_scene));
-	(*scene)->world = saffe_calloc(*scene, 1, sizeof(t_world));
+		end(*scene, ERR_OPEN_FILE, file);
+	*scene = saffe_calloc(*scene, 1, sizeof(t_scene), NULL);
+	(*scene)->world = saffe_calloc(*scene, 1, sizeof(t_world), NULL);
 	while (TRUE)
 	{
 		line = get_next_line(file_fd);
