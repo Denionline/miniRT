f26f7a672bd8725d26c65bdbcd88765ecb62ec06
denionline <danielximenes164@gmail.com>
2026-01-30 15:21:00 +0000

    fix: Update parameter counts for scene objects to ensure accurate parsing

--- include/parse.h
+++ include/parse.h
@@ -4,13 +4,13 @@
 #include "scene.h"
 
 enum N_PARAMS {
-	NPARAM_TUPLE = 3,
-	NPARAM_AMBLIGHT = 2,
-	NPARAM_CAMERA = 3,
-	NPARAM_LIGHT = 3,
-	NPARAM_SPHERE = 3,
-	NPARAM_PLANE = 3,
-	NPARAM_CYLINDER = 5,
+	NPARAM_TUPLE = 4,
+	NPARAM_AMBLIGHT = 3,
+	NPARAM_CAMERA = 4,
+	NPARAM_LIGHT = 4,
+	NPARAM_SPHERE = 4,
+	NPARAM_PLANE = 4,
+	NPARAM_CYLINDER = 6,
 };
 
 void	parse_scene(t_scene **scene, char *file);
