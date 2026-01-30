55a9e6f35943d1556f0cc273e52b7e559743e0cf
denionline <danielximenes164@gmail.com>
2026-01-30 14:59:41 +0000

    fix: Update error handling function signatures and enum definitions

--- include/end.h
+++ include/end.h
@@ -3,15 +3,15 @@
 
 enum ERROR_CODE {
 	ERR_MALLOC = 42,
+	ERR_OPEN_FILE = 43,
 	ERR_NOT_ENOUGH_ARGS,
-	ERR_OPEN_FILE,
 	ERR_INVALID_TUPLE,
-	ERR_UNKNOWN_IDENTIFIER,
 	ERR_OUT_OF_RANGE,
 	ERR_INVALID_CHAR,
+	ERR_UNKNOWN_IDENTIFIER = 53,
 };
 
-void	end(t_scene *scene, enum ERROR_CODE status_code);
-void	print_error(enum ERROR_CODE error_code);
+void	end(t_scene *scene, enum ERROR_CODE status_code, char *description);
+void	print_error(enum ERROR_CODE error_code, char *description);
 
 #endif
