16ade680002e4dbe7ee513224c9572741a0d840b
denionline <danielximenes164@gmail.com>
2026-01-30 12:10:49 +0000

    fix: Refactor end function to use scene_cleaner for resource management

--- src/exit/end.c
+++ src/exit/end.c
@@ -1,6 +1,30 @@
 #include "head.h"
 
-int	end(t_scene *scene, enum ERROR_CODE error_code, char *description)
+static void	scene_cleaner(t_scene *scene)
 {
+	size_t	i;
 
+	if (scene)
+	{
+		if (scene->amblight)
+		free(scene->amblight);
+		if (scene->camera)
+			free(scene->camera);
+		if (scene->world && scene->world->n_objs)
+		{
+			i = 0;
+			while (i < scene->world->n_objs)
+				free(scene->world->objects[i++]);
+			free(scene->world->objects);
+		}
+		free(scene);
+	}
+}
+
+void	end(t_scene *scene, enum ERROR_CODE status_code)
+{
+	scene_cleaner(scene);
+	if (status_code)
+		print_error(status_code);
+	exit(status_code);
 }
