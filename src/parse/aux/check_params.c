4cdca974e9dc9d48e62f1db0edd7e3561246482f
denionline <danielximenes164@gmail.com>
2026-02-01 19:01:33 +0000

    fix: Update timestamps in header comments and improve formatting in check_params and parse_plane files

--- src/parse/aux/check_params.c
+++ src/parse/aux/check_params.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 14:27:14 by dximenes          #+#    #+#             */
-/*   Updated: 2026/02/01 14:27:15 by dximenes         ###   ########.fr       */
+/*   Updated: 2026/02/01 18:59:05 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,10 +83,8 @@ void	check_params(t_scene *scene, char *s, int nparams, int exists)
 	while (s && s[i])
 	{
 		if (!ft_isdigit(s[i]) && !ft_isspace(s[i]))
-		{
 			if (s[i] != '.' && s[i] != ',' && s[i] != '+' && s[i] != '-')
 				break ;
-		}
 		i++;
 	}
 	if (s[i] && !is_pattern(s + i))
--- src/parse/aux/parse_plane.c
+++ src/parse/aux/parse_plane.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_plane.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 18:59:49 by dximenes          #+#    #+#             */
+/*   Updated: 2026/02/01 19:01:19 by dximenes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "head.h"
 
 static int	error_check(t_object *plane)
@@ -14,14 +26,17 @@ static int	error_check(t_object *plane)
 static t_pattern	check_pattern(char *line)
 {
 	if (line[0] == 'c' && line[1] == 'k')
-		return (checkboard_pattern(color_float(1, 1, 1), color_float(0, 0, 0), PLANE));
+		return (checkboard_pattern(color_float(1, 1, 1),
+				color_float(0, 0, 0), PLANE));
 	if (line[0] == 's' && line[1] == 't')
-		return (stripe_pattern(color_float(1, 1, 1), color_float(0, 0, 0), PLANE));
+		return (stripe_pattern(color_float(1, 1, 1),
+				color_float(0, 0, 0), PLANE));
 	if (line[0] == 'r' && line[1] == 'g')
-		return (ring_pattern(color_float(1, 1, 1), color_float(1, 0, 0), PLANE));
+		return (ring_pattern(color_float(1, 1, 1),
+				color_float(1, 0, 0), PLANE));
 	if (line[0] == 'g' && line[1] == 'r')
-		return (gradient_pattern(color_float(1, 0, 0), \
-		color_float(252 / 255.0f, 15 / 255.0f, 192 / 255.0f), PLANE));
+		return (gradient_pattern(color_float(1, 0, 0),
+				color_float(252 / 255.0f, 15 / 255.0f, 192 / 255.0f), PLANE));
 	return ((t_pattern){});
 }
 
@@ -33,18 +48,17 @@ static int	check_cond(size_t *paramc, t_object *new_plane, char *line)
 		new_plane->position = string_to_tuple(line, POINT);
 	else if ((*paramc) == 1 && ++(*paramc))
 		new_plane->normal = normalize(
-			string_to_tuple(line, VECTOR)
-		);
+				string_to_tuple(line, VECTOR));
 	else if ((*paramc) == 2 && ++(*paramc))
 		new_plane->material = material(
-			multiply_tuple(
-				string_to_tuple(line, COLOR),
-				1.0f / 255.0f
-			)
-		);
+				multiply_tuple(
+					string_to_tuple(line, COLOR),
+					1.0f / 255.0f
+					)
+				);
 	else if ((*paramc) == 3 && ++(*paramc))
 	{
-		if(*line != '\n')
+		if (*line != '\n')
 		{
 			new_plane->material.pattern = check_pattern(line);
 			if (!new_plane->material.pattern.has_pattern)
@@ -88,9 +102,8 @@ t_object	*parse_plane(t_scene *scene, char *line)
 	p = new_plane->position;
 	new_plane->transform = geral_rotation(new_plane->normal);
 	new_plane->transform = multiply_matrix(
-		translation(p.x, p.y, p.z),
-		new_plane->transform
-	);
+			translation(p.x, p.y, p.z),
+			new_plane->transform);
 	new_plane->transform = inverse(new_plane->transform);
 	new_plane->material.pattern.transform = new_plane->transform;
 	return (new_plane);
