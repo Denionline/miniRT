99234bea307e135459f2fe620ff25b0bf6542c27
denionline <danielximenes164@gmail.com>
2026-02-01 15:41:59 +0000

    fix: Refactor construct_color function for improved readability and remove duplicate declaration

--- src/canvas/ft_pixel_put.c
+++ src/canvas/ft_pixel_put.c
@@ -6,17 +6,22 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 14:32:29 by dximenes          #+#    #+#             */
-/*   Updated: 2026/02/01 15:38:08 by dximenes         ###   ########.fr       */
+/*   Updated: 2026/02/01 15:41:22 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "head.h"
 
-static unsigned int	construct_color(t_tuple color, int endian);
+static unsigned int	construct_color(t_tuple color, int endian)
+{
+	if (!endian)
+		return (color.r << 16 | color.g << 8 | color.b);
+	return (color.b << 16 | color.g << 8 | color.r);
+}
 
 void	pixel_put(t_canvas *canvas, int x, int y, t_tuple color)
 {
-	char *dst;
+	char	*dst;
 
 	if (x >= canvas->hsize || y >= canvas->vsize
 		|| x < 0 || y < 0)
@@ -24,12 +29,5 @@ void	pixel_put(t_canvas *canvas, int x, int y, t_tuple color)
 	dst = canvas->address;
 	dst += (canvas->vsize - y) * canvas->sl;
 	dst += x * (canvas->bpp / 8);
-	*(unsigned int*)dst = construct_color(color, canvas->endian);
-}
-
-static unsigned int	construct_color(t_tuple color, int endian)
-{
-	if (!endian)
-		return ( color.r << 16 | color.g << 8 |  color.b);
-	return ( color.b << 16 |  color.g << 8 |  color.r);
+	*(unsigned int *)dst = construct_color(color, canvas->endian);
 }
