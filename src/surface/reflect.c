550f2298668622d6884430ba787b297fc4de4c96
denionline <danielximenes164@gmail.com>
2026-02-01 15:49:30 +0000

    fix: Update timestamp in header comment and improve formatting of reflect function

--- src/surface/reflect.c
+++ src/surface/reflect.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 14:33:20 by dximenes          #+#    #+#             */
-/*   Updated: 2026/02/01 14:33:21 by dximenes         ###   ########.fr       */
+/*   Updated: 2026/02/01 15:49:21 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 
 t_tuple	reflect(t_tuple in, t_tuple normal)
 {
-	return subtract_tuples(
-		in,
-		multiply_tuple(
-			normal,
-			2 * dot(in, normal)
-		)
-	);
+	return (subtract_tuples(
+			in,
+			multiply_tuple(
+				normal,
+				2 * dot(in, normal)
+			)
+		));
 }
-
