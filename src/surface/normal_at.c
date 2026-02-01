2eade1216eedded03ab016505be552fe17f2bb35
denionline <danielximenes164@gmail.com>
2026-02-01 15:48:46 +0000

    fix: Update header comment timestamp and adjust variable declarations for consistency

--- src/surface/normal_at.c
+++ src/surface/normal_at.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 14:33:20 by dximenes          #+#    #+#             */
-/*   Updated: 2026/02/01 14:33:21 by dximenes         ###   ########.fr       */
+/*   Updated: 2026/02/01 15:48:18 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 t_tuple	normal_at(t_object *object, t_tuple p)
 {
-	t_tuple object_point;
-	t_tuple new_object;
-	t_matrix inv;
+	t_tuple		object_point;
+	t_tuple		new_object;
+	t_matrix	inv;
 
 	if (!object)
 		return (tuple(0, 0, 0, 0));
-	// if (!is_tuple_empty(object->normal))
-	// 	return (object->normal);
 	inv = (object->transform);
 	object_point = multiply_matrix_tuple(inv, p);
 	new_object = local_normal_at(object, object_point);
