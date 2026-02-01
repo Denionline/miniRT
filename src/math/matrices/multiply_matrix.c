0ed154448e7114f768af1eb96f483ab700281894
denionline <danielximenes164@gmail.com>
2026-02-01 18:53:13 +0000

    fix: Update timestamps in header comments and improve formatting in matrix multiplication files

--- src/math/matrices/multiply_matrix.c
+++ src/math/matrices/multiply_matrix.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 14:30:13 by dximenes          #+#    #+#             */
-/*   Updated: 2026/02/01 14:30:14 by dximenes         ###   ########.fr       */
+/*   Updated: 2026/02/01 18:51:47 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 t_matrix	multiply_matrix(t_matrix m1, t_matrix m2)
 {
-	t_matrix	m;
-	unsigned	r;
-	unsigned	c;
+	t_matrix		m;
+	unsigned int	r;
+	unsigned int	c;
 
 	m.size = m1.size;
 	r = 0;
--- src/math/matrices/multiply_matrix_tuple.c
+++ src/math/matrices/multiply_matrix_tuple.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 14:30:14 by dximenes          #+#    #+#             */
-/*   Updated: 2026/02/01 18:51:06 by dximenes         ###   ########.fr       */
+/*   Updated: 2026/02/01 18:53:02 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 t_tuple	multiply_matrix_tuple(t_matrix matrix, t_tuple t)
 {
-	const float	(*m)[4] = matrix.matrix;
+	const	float	(*m)[4] = matrix.matrix;
 
 	return (
 		(t_tuple){
-			.x = m[0][0] * t.x + m[0][1] * t.y + m[0][2] * t.z + m[0][3] * t.w,
-			.y = m[1][0] * t.x + m[1][1] * t.y + m[1][2] * t.z + m[1][3] * t.w,
-			.z = m[2][0] * t.x + m[2][1] * t.y + m[2][2] * t.z + m[2][3] * t.w,
-			.w = m[3][0] * t.x + m[3][1] * t.y + m[3][2] * t.z + m[3][3] * t.w
+		.x = m[0][0] * t.x + m[0][1] * t.y + m[0][2] * t.z + m[0][3] * t.w,
+		.y = m[1][0] * t.x + m[1][1] * t.y + m[1][2] * t.z + m[1][3] * t.w,
+		.z = m[2][0] * t.x + m[2][1] * t.y + m[2][2] * t.z + m[2][3] * t.w,
+		.w = m[3][0] * t.x + m[3][1] * t.y + m[3][2] * t.z + m[3][3] * t.w
 		}
 	);
 }
