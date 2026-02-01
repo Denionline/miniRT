12f1fd765f99b9e39a56bf2f78988f9d6b2e739f
denionline <danielximenes164@gmail.com>
2026-02-01 18:58:51 +0000

    fix: Update timestamps in header comments and improve formatting in parse_camera and parse_cylinder files

--- src/parse/aux/parse_cylinder.c
+++ src/parse/aux/parse_cylinder.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_cylinder.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 18:55:52 by dximenes          #+#    #+#             */
+/*   Updated: 2026/02/01 18:58:42 by dximenes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "head.h"
 
 static int	error_check(t_object *cylinder)
@@ -14,14 +26,29 @@ static int	error_check(t_object *cylinder)
 static t_pattern	check_pattern(char *line)
 {
 	if (line[0] == 'c' && line[1] == 'k')
-		return (checkboard_pattern(color_float(1, 1, 1), color_float(0, 0, 0), CYLINDER));
+		return (checkboard_pattern(
+				color_float(1, 1, 1),
+				color_float(0, 0, 0),
+				CYLINDER)
+		);
 	if (line[0] == 's' && line[1] == 't')
-		return (stripe_pattern(color_float(1, 1, 1), color_float(0, 0, 0), CYLINDER));
+		return (stripe_pattern(
+				color_float(1, 1, 1),
+				color_float(0, 0, 0),
+				CYLINDER)
+		);
 	if (line[0] == 'r' && line[1] == 'g')
-		return (ring_pattern(color_float(1, 1, 1), color_float(1, 0, 0), CYLINDER));
+		return (ring_pattern(
+				color_float(1, 1, 1),
+				color_float(1, 0, 0),
+				CYLINDER)
+		);
 	if (line[0] == 'g' && line[1] == 'r')
-		return (gradient_pattern(color_float(1, 0, 0), \
-		color_float(252 / 255.0f, 15 / 255.0f, 192 / 255.0f), CYLINDER));
+		return (gradient_pattern(
+				color_float(1, 0, 0),
+				color_float(252 / 255.0f, 15 / 255.0f, 192 / 255.0f),
+				CYLINDER)
+		);
 	return ((t_pattern){});
 }
 
@@ -33,17 +60,17 @@ static int	check_cond(size_t *paramc, t_object *new_cylinder, char *line)
 		new_cylinder->position = string_to_tuple(line, POINT);
 	else if ((*paramc) == 1 && ++(*paramc))
 		new_cylinder->normal = normalize(
-			string_to_tuple(line, VECTOR));
+				string_to_tuple(line, VECTOR));
 	else if ((*paramc) == 2 && ++(*paramc))
 		new_cylinder->diameter = ft_atof(line);
 	else if ((*paramc) == 3 && ++(*paramc))
 		new_cylinder->height = ft_atof(line);
 	else if ((*paramc) == 4 && ++(*paramc))
 		new_cylinder->material = material(multiply_tuple(
-			string_to_tuple(line, COLOR), 1.0f / 255.0f));
+					string_to_tuple(line, COLOR), 1.0f / 255.0f));
 	else if ((*paramc) == 5 && ++(*paramc))
 	{
-		if(*line != '\n')
+		if (*line != '\n')
 		{
 			new_cylinder->material.pattern = check_pattern(line);
 			if (!new_cylinder->material.pattern.has_pattern)
@@ -76,7 +103,7 @@ t_object	*parse_cylinder(t_scene *scene, char *line)
 	t_tuple		p;
 	float		radius;
 	int			error_code;
-	
+
 	check_params(scene, line, NPARAM_CYLINDER, 0);
 	new_cylinder = saffe_calloc(scene, line, 1, sizeof(t_object));
 	new_cylinder->type = CYLINDER;
@@ -89,13 +116,11 @@ t_object	*parse_cylinder(t_scene *scene, char *line)
 	radius = new_cylinder->diameter * 0.5f;
 	new_cylinder->transform = geral_rotation(new_cylinder->normal);
 	new_cylinder->transform = multiply_matrix(
-		scaling(radius, new_cylinder->height, radius),
-		new_cylinder->transform
-	);
+			scaling(radius, new_cylinder->height, radius),
+			new_cylinder->transform);
 	new_cylinder->transform = multiply_matrix(
-		translation(p.x, p.y, p.z),
-		new_cylinder->transform
-	);
+			translation(p.x, p.y, p.z),
+			new_cylinder->transform);
 	new_cylinder->transform = inverse(new_cylinder->transform);
 	return (new_cylinder);
 }
