db2ce85874a1658bd0258f5a02a1c8f9d3b3fbac
denionline <danielximenes164@gmail.com>
2026-02-01 17:40:29 +0000

    fix: Fixed identantations on camera

--- src/world/camera.c
+++ src/world/camera.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 14:33:30 by dximenes          #+#    #+#             */
-/*   Updated: 2026/02/01 14:33:31 by dximenes         ###   ########.fr       */
+/*   Updated: 2026/02/01 17:40:05 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_camera	camera(float hsize, float vsize, float fov)
 	float		half_v;
 
 	half_v = tanf(fov * 0.5f);
-	cam = (t_camera) 
+	cam = (t_camera)
 	{
 		.hsize = hsize,
 		.vsize = vsize,
@@ -30,12 +30,12 @@ t_camera	camera(float hsize, float vsize, float fov)
 	if (aspect >= 1)
 	{
 		cam.half_w = half_v;
-		cam.half_h = half_v / aspect; 
+		cam.half_h = half_v / aspect;
 	}
 	else
 	{
 		cam.half_w = half_v * aspect;
-		cam.half_h = half_v; 
+		cam.half_h = half_v;
 	}
 	cam.pixel_size = cam.half_w * 2.0f / cam.hsize;
 	return (cam);
