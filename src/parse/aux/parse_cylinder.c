ed48dfc7df33ae8b2160a4f2bd2085690aa3a26a
Denionline <danielximenes164@gmail.com>
2026-02-18 11:58:46 +0000

    fix: refactor error_check and fill_values functions for improved clarity and consistency in cylinder parsing

--- src/parse/aux/parse_cylinder.c
+++ src/parse/aux/parse_cylinder.c
@@ -6,22 +6,21 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 18:55:52 by dximenes          #+#    #+#             */
-/*   Updated: 2026/02/18 10:42:38 by dximenes         ###   ########.fr       */
+/*   Updated: 2026/02/18 11:58:20 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "head.h"
 
-static int	error_check(t_object *cylinder, int *error_code)
+static int	error_check(t_object *cylinder)
 {
-	*error_code = 0;
 	if (cylinder->position.error_code)
-		*error_code = cylinder->position.error_code;
+		return (cylinder->position.error_code);
 	if (cylinder->normal.error_code)
-		*error_code = cylinder->normal.error_code;
+		return (cylinder->normal.error_code);
 	if (cylinder->material.color.error_code)
-		*error_code = cylinder->material.color.error_code;
-	return (*error_code);
+		return (cylinder->material.color.error_code);
+	return (0);
 }
 
 static t_pattern	check_pattern(char *line)
@@ -53,35 +52,35 @@ static t_pattern	check_pattern(char *line)
 	return ((t_pattern){});
 }
 
-static int	check_cond(size_t *paramc, t_object *new_cy, char *line)
+static int	check_cond(size_t *paramc, t_object *new_cylinder, char *line)
 {
 	if ((*paramc) == 0 && *line == 'c')
 		line += 2;
 	else if ((*paramc) == 0 && ++(*paramc))
-		new_cy->position = string_to_tuple(line, POINT);
+		new_cylinder->position = string_to_tuple(line, POINT);
 	else if ((*paramc) == 1 && ++(*paramc))
-		new_cy->normal = normalize(
+		new_cylinder->normal = normalize(
 				string_to_tuple(line, VECTOR));
 	else if ((*paramc) == 2 && ++(*paramc))
-		new_cy->diameter = ft_atof(line);
+		new_cylinder->diameter = ft_atof(line);
 	else if ((*paramc) == 3 && ++(*paramc))
-		new_cy->height = ft_atof(line);
+		new_cylinder->height = ft_atof(line);
 	else if ((*paramc) == 4 && ++(*paramc))
-		new_cy->material = material(multiply_tuple(
+		new_cylinder->material = material(multiply_tuple(
 					string_to_tuple(line, COLOR), 1.0f / 255.0f));
 	else if ((*paramc) == 5 && ++(*paramc))
 	{
 		if (*line != '\n')
 		{
-			new_cy->material.pattern = check_pattern(line);
-			if (!new_cy->material.pattern.has_pattern)
+			new_cylinder->material.pattern = check_pattern(line);
+			if (!new_cylinder->material.pattern.has_pattern)
 				return (0);
 		}
 	}
 	return (1);
 }
 
-static int	fill_values(t_object *new_cy, char *line)
+static int	fill_values(t_object *new_cylinder, char *line)
 {
 	size_t		paramc;
 
@@ -90,7 +89,7 @@ static int	fill_values(t_object *new_cy, char *line)
 	{
 		while (ft_isspace(*line))
 			line++;
-		if (!check_cond(&paramc, new_cy, line))
+		if (!check_cond(&paramc, new_cylinder, line))
 			return (0);
 		while (*line && !ft_isspace(*(line++)))
 			;
@@ -100,29 +99,24 @@ static int	fill_values(t_object *new_cy, char *line)
 
 t_object	*parse_cylinder(t_scene *scene, char *line)
 {
-	t_object	*new_cy;
+	t_object	*new_cylinder;
 	t_tuple		p;
 	float		radius;
 	int			error_code;
 
 	check_params(scene, line, NPARAM_CYLINDER, 0);
-	new_cy = saffe_calloc(scene, line, 1, sizeof(t_object));
-	new_cy->type = CYLINDER;
-	error_code = ERR_INVALID_CHAR;
-	if (!fill_values(new_cy, line) || error_check(new_cy, &error_code))
-	{
-		free(new_cy);
+	new_cylinder = saffe_calloc(scene, line, 1, sizeof(t_object));
+	new_cylinder->type = CYLINDER;
+	if (!fill_values(new_cylinder, line))
+		end(scene, ERR_INVALID_CHAR, line, TRUE);
+	error_code = error_check(new_cylinder);
+	if (error_code)
 		end(scene, error_code, line, TRUE);
-	}
-	p = new_cy->position;
-	radius = new_cy->diameter * 0.5f;
-	new_cy->transform = geral_rotation(new_cy->normal);
-	new_cy->transform = multiply_matrix(
-			scaling(radius, new_cy->height, radius),
-			new_cy->transform);
-	new_cy->transform = multiply_matrix(
-			translation(p.x, p.y, p.z),
-			new_cy->transform);
-	new_cy->transform = inverse(new_cy->transform);
-	return (new_cy);
+	p = new_cylinder->position;
+	radius = new_cylinder->diameter * 0.5f;
+	new_cylinder->transform = multiply_matrix(translation(p.x, p.y, p.z), \
+		multiply_matrix(geral_rotation(new_cylinder->normal),\
+		scaling(radius, new_cylinder->height, radius)));
+	new_cylinder->transform = inverse(new_cylinder->transform);
+	return (new_cylinder);
 }
