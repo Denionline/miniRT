bd7dca6dc2bc764c15c6b57080039276b7e0545a
Denionline <danielximenes164@gmail.com>
2026-02-18 10:26:32 +0000

    fix: simplify error_check function and streamline error handling in parse_amblight

--- src/parse/aux/parse_amblight.c
+++ src/parse/aux/parse_amblight.c
@@ -6,18 +6,17 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 19:05:06 by dximenes          #+#    #+#             */
-/*   Updated: 2026/02/18 10:22:43 by dximenes         ###   ########.fr       */
+/*   Updated: 2026/02/18 10:26:25 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "head.h"
 
-static int	error_check(t_amblight *amblight, int *error_code)
+static int	error_check(t_amblight *amblight)
 {
-	*error_code = 0;
 	if (amblight->color.error_code)
-		*error_code = (amblight->color.error_code);
-	return (*error_code);
+		return (amblight->color.error_code);
+	return (0);
 }
 
 static void	fill_values(t_amblight *amblight, char *line)
@@ -49,8 +48,9 @@ void	parse_amblight(t_scene *scene, t_amblight **amblight, char *line)
 	check_params(scene, line, NPARAM_AMBLIGHT, (*amblight) != NULL);
 	*amblight = saffe_calloc(scene, line, 1, sizeof(t_amblight));
 	fill_values(*amblight, line);
-	error_code = ERR_OUT_OF_RANGE;
-	if ((*amblight)->ratio < 0.00f || (*amblight)->ratio > 1.00f ||
-		error_check(*amblight, &error_code))
+	if ((*amblight)->ratio < 0.00f || (*amblight)->ratio > 1.00f)
+		end(scene, ERR_OUT_OF_RANGE, line, TRUE);
+	error_code = error_check(*amblight);
+	if (error_code)
 		end(scene, error_code, line, TRUE);
 }
